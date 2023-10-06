91bc62d600502820071520357d68943185b762bc
Tuhin <tuhinbr99@gmail.com>
2023-10-06 06:14:40 +0530

    Inbox contract

---
+++ inbox-contract/deploy.js
@@ -0,0 +1,12 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const {interface, bytecode} = require('./compile');
+//updated web3 and hdwallet-provider imports added for convenience
+
+// deploy code will go here
+
+const provider = new HDWalletProvider(
+
+)
+
+
